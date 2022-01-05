b3f5fe4a9071064c51fe006968ec84e0b861a45e
AngadK89 <93425810+AngadK89@users.noreply.github.com>
2022-01-05 17:38:11 +0530

    Updating Cart Total
    Updating cart total over cart icon by adding a call to get_cart_items into views.py from the Order model in models.py. This allows the total cart items to be dynamically updated and passed into the main.html template, which all templates inherit from.

--- static/js/cart.js
+++ static/js/cart.js
@@ -30,8 +30,12 @@ function updateUserOrder(productId, action){
         body: JSON.stringify({'productId': productId, 'action': action}), 
     })
 
-    .then((response) => {return response.json()})
+    .then((response) => {
+        return response.json();
+    })
 
-
-    .then((data) => {console.log('Data: ', data)})
+    .then((data) => {
+        console.Log('Data: ', data)
+        location.reload()
+    });
 }
--- store/views.py
+++ store/views.py
@@ -8,27 +8,33 @@ def querying_data(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         #Create empty cart as of now for guest (non-logged in) users
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0}
-    return items, order
+        cartItems = order['get_cart_items']
+    return items, order, cartItems
 
 
 def store(request):
+    items, order, cartItems = querying_data(request)
     products = Product.objects.all()
-    context = {'products': products}
+    context = {'products': products, 'cartItems': cartItems}
     return render(request, 'store/store.html', context)
 
+
 def cart(request):
-    items, order = querying_data(request)
-    context = {'items': items, 'order': order}
+    items, order, cartItems = querying_data(request)
+    context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
 
+
 def checkout(request):
-    items, order = querying_data(request)
-    context = {'items': items, 'order': order}
+    items, order, cartItems = querying_data(request)
+    context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/checkout.html', context)
+
 
 def updateItem(request):
     data = json.loads(request.body)

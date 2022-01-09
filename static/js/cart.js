8bf0ddf024711aac06534077938cdb592c630a17
AngadK89 <93425810+AngadK89@users.noreply.github.com>
2022-01-09 23:24:38 +0530

    Updating Items to Cart Cookie
    Writing a function to updating the product ID and quantity of a product in the JSON cart variable when the 'add to cart' button is clicked.
    
    Uses a nested JSON object to track the product added, and the quantity of said product in the cart.
    
    The updated JSON type 'cart' variable is then published to the cookies again to be stored in the backend, so it can be saved.

--- static/js/cart.js
+++ static/js/cart.js
@@ -7,13 +7,38 @@ for(var i = 0; i < updateBtns.length; i++){
         console.log("product ID: ", productId, "Action: ", action)
 
         console.log("User: ", user)
-        if (user === 'AnonymousUser') {
-            console.log("Not logged in")
+        if (user == 'AnonymousUser') {
+            addCookieItem(productId, action)
         }
         else {
             updateUserOrder(productId, action)
         }
     })
+}
+
+
+function addCookieItem(productId, action) {
+    if (action == "add") {
+        if (cart[productId] == undefined) {
+            cart[productId] = {'quantity': 1}
+        }
+        else {
+            cart[productId]['quantity'] += 1
+        }
+    }
+
+    if (action == "remove") {
+        cart[productId]["quantity"] -= 1
+
+        if (cart[productId]['quantity'] <= 0) {
+            console.log("Item should be deleted.")
+            delete cart[productId];
+        }
+    }
+
+    console.log("Cart: ", cart)
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+    location.reload()
 }
 
 

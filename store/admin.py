455aa1c15c82a36a456c79ec4fc4d6c176d95790
AngadK89 <93425810+AngadK89@users.noreply.github.com>
2022-01-11 14:44:05 +0530

    Changing Admin Page Functionality
    Changing functionality of admin page to:
    1. Make some fields/models uneditable by the admin. For instance, the admin should not be able to change the shipping address details, customer information, or order details of the customer. Thus, these fields are read only on the admin site.
    
    2. Giving the admin the ability to search for specific instances of a model.
    
    3. Allowing admin ability to order/sort/filter using specific parameters, to make the functionality easier for them.

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,46 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Customer)
-admin.site.register(Product)
-admin.site.register(Order)
+
+class CustomerAdmin(admin.ModelAdmin):
+    fields = ('user', 'name', 'email')
+    readonly_fields = ('user', 'name', 'email')
+    list_display = ('user', 'name', 'email')
+    search_fields = ['name', 'email', 'user__username']
+
+
+class ProductAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Product Details", {'fields': ['name', 'desc', 'category', 'image']}),
+        (None, {'fields': ['price', 'stock']}),
+        ]
+
+    list_display = ('name', 'category', 'stock')
+    list_filter = ['category']
+    search_fields = ['name']
+    ordering = ['name', ]
+
+class OrderAdmin(admin.ModelAdmin):
+    fields = ['customer', 'transaction_id', 'date_ordered', 'status']
+    readonly_fields = ['customer', 'transaction_id', 'date_ordered']
+    list_display = ['transaction_id', 'date_ordered', 'status']
+    list_filter = ['status']
+    ordering = ['date_ordered']
+
+
+class ShippingAddressAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ('customer', 'order', 'date_added')}),
+        ('Shipping Details', {'fields': ('address', 'city', 'state', 'zipcode')}),
+    ]
+
+    readonly_fields = ['customer', 'order', 'address', 'city', 'state', 'zipcode', 'date_added']
+    list_display = ['address', 'customer', 'date_added']
+    search_fields = ['address', 'city', 'state', 'zipcode', 'customer__name']
+
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderItem)
-admin.site.register(ShippingAddress)
+admin.site.register(ShippingAddress, ShippingAddressAdmin)
